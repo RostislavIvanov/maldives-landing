34adf9d46e619e38e573def662f81ce6c33663e4
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2023-11-11 00:07:20 +0300

    Инициализация проекта

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainScreenBlock from "./Blocks/MainScreenBlock/MainScreenBlock.tsx";
+
+const App = () => {
+    return (
+        <>
+            <MainScreenBlock/>
+        </>
+    );
+};
+
+export default App;
---
+++ src/Blocks/MainScreenBlock/MainScreenBlock.tsx
@@ -0,0 +1,9 @@
+const MainScreenBlock = () => {
+    return (
+        <div className={'bg-amber-400'}>
+            начало
+        </div>
+    );
+};
+
+export default MainScreenBlock;
