aa79cebc8d4cf58645f280f19a078feb1400838b
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-01-04 15:07:31 +0300

    Доработка блока экскурсий

--- src/Blocks/Excursions/Excursions.tsx
+++ src/Blocks/Excursions/Excursions.tsx
@@ -2,45 +2,50 @@ import classes from './Excursions.module.css'
 import BlockTitle from "~/components/UI/BlockTitle/BlockTitle.tsx";
 import ExcursionItem from "~/components/ExcursionItem/ExcursionItem.tsx";
 import beach from '../../assets/images/Beach.jpg'
+import snorkeling from '../../assets/images/snorkeling.jpg'
+import dolphine from '../../assets/images/dolphine-tour.jpg'
+import ship from '../../assets/images/Ship.jpg'
+import whaleShark from '../../assets/images/whale-shark.jpg'
+import shark from '../../assets/images/sharks.jpg'
 
 const Excursions = () => {
     return (
         <div className={classes.content}>
             <div className={classes.wrapper}>
                 <div className={classes.titleWrapper}>
-                    <BlockTitle>Экскурсии</BlockTitle>
+                    <BlockTitle label={'Насладитесь чудесами нашего острова'}>Экскурсии</BlockTitle>
                 </div>
 
                 <div className={'flex justify-between flex-wrap items-end text-white'}>
                     <ExcursionItem
-                        title={'Тур с китовыми акулами'}
+                        title={'3 точки снорклинга'}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
+                        imageUrl={snorkeling}
+                    />
+                    <ExcursionItem
+                        title={'Ночная рыбалка'}
                         description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
                         imageUrl={beach}
                     />
                     <ExcursionItem
-                        title={'Тур с китовыми акулами'}
+                        title={'Тур с дельфинами'}
                         description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
-                        imageUrl={beach}
+                        imageUrl={dolphine}
+                    />
+                    <ExcursionItem
+                        title={'Приключение с акулами'}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
+                        imageUrl={shark}
+                    />
+                    <ExcursionItem
+                        title={'Путешествие на затонувший корабль'}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
+                        imageUrl={ship}
                     />
                     <ExcursionItem
                         title={'Тур с китовыми акулами'}
                         description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
-                        imageUrl={beach}
-                    />
-                    <ExcursionItem
-                        title={'Тур с китовыми акулами'}
-                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
-                        imageUrl={beach}
-                    />
-                    <ExcursionItem
-                        title={'Тур с китовыми акулами'}
-                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
-                        imageUrl={beach}
-                    />
-                    <ExcursionItem
-                        title={'Тур с китовыми акулами'}
-                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
-                        imageUrl={beach}
+                        imageUrl={whaleShark}
                     />
                 </div>
             </div>
