f960b148734967901640a3936a9943b5e3592af5
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-01-04 18:32:45 +0300

    Изменение фотографии в блоке экскурсий

--- src/Blocks/Excursions/Excursions.tsx
+++ src/Blocks/Excursions/Excursions.tsx
@@ -1,7 +1,7 @@
 import classes from './Excursions.module.css'
 import BlockTitle from "~/components/UI/BlockTitle/BlockTitle.tsx";
 import ExcursionItem from "~/components/ExcursionItem/ExcursionItem.tsx";
-import beach from '../../assets/images/Beach.jpg'
+import fishing from '../../assets/images/night-fishing.jpg'
 import snorkeling from '../../assets/images/snorkeling.jpg'
 import dolphine from '../../assets/images/dolphine-tour.jpg'
 import ship from '../../assets/images/Ship.jpg'
@@ -25,7 +25,7 @@ const Excursions = () => {
                     <ExcursionItem
                         title={'Ночная рыбалка'}
                         description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, itaque, porro. Ab accusamus architecto consequuntur, cum explicabo impedit laudantium maiores praesentium sapiente tempora veniam voluptatem. Fugiat iste iusto pariatur reprehenderit?'}
-                        imageUrl={beach}
+                        imageUrl={fishing}
                     />
                     <ExcursionItem
                         title={'Тур с дельфинами'}
