f2e378353332d8b4e10bcea4cc12079b2609f5d4
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-01-05 23:53:23 +0300

    Доработка меню навигации

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,14 +7,14 @@ const Header = () => {
             <nav className={classes.nav}>
                 <ul className={classes.nav__left}>
                     <li><a href="#about">Об острове</a></li>
-                    <li><a href="#accommodation">Размещение</a></li>
                     <li><a href="#excursions">Экскурсии</a></li>
+                    <li><a href="#accommodation">Размещение</a></li>
                 </ul>
                 <div className={classes.nav__logo}>
                     <img src={logo} alt="logo"/>
                 </div>
                 <ul className={classes.nav__right}>
-                    <li><a href="#interesting">Выгодные предложения</a></li>
+                    <li><a href="#interesting">Наши особенности</a></li>
                     <li><a href="#prices">Цены и контакты</a></li>
                 </ul>
             </nav>
