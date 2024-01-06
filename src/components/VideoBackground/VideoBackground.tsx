cb6c78cf61df6bbc711355e75d043db6d6c72487
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-01-06 12:32:12 +0300

    Временная замена видеофона на картинку

--- src/components/VideoBackground/VideoBackground.tsx
+++ src/components/VideoBackground/VideoBackground.tsx
@@ -1,16 +1,18 @@
 import classes from "./VideoBackground.module.css";
-import MyVideo from "../../assets/video/Maldives Travel Guide (720p).mp4";
+// import MyVideo from "../../assets/video/Maldives Travel Guide (720p).mp4";
+import back from '../../assets/images/back.jpg'
 import { FC } from "react";
 const VideoBackground: FC = () => {
     return (
         <div className={classes.video}>
-            <video
-                className={classes.video__media}
-                   src={MyVideo}
-                   autoPlay={true}
-                   muted={true}
-                   loop>
-            </video>
+            {/*<video*/}
+            {/*    className={classes.video__media}*/}
+            {/*       src={MyVideo}*/}
+            {/*       autoPlay={true}*/}
+            {/*       muted={true}*/}
+            {/*       loop>*/}
+            {/*</video>*/}
+            <img className={classes.video} src={back} alt=""/>
         </div>
     );
 };
