1822cbe35f1e38bde12c41bce1b117e54204b31a
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-01-24 18:38:07 +0300

    Убрал видеофон

--- src/Blocks/MainScreenBlock/MainScreenBlock.tsx
+++ src/Blocks/MainScreenBlock/MainScreenBlock.tsx
@@ -1,11 +1,11 @@
 import classes from "./MainScreenBlock.module.css";
-import VideoBackground from "../../components/VideoBackground/VideoBackground.tsx";
+// import VideoBackground from "../../components/VideoBackground/VideoBackground.tsx";
 import MainScreenContent from "../../components/MainScreenContent/MainScreenContent.tsx";
 
 const MainScreenBlock = () => {
     return (
         <div className={classes.intro}>
-            <VideoBackground/>
+            {/*<VideoBackground/>*/}
             <MainScreenContent/>
         </div>
 
