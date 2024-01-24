c1e24bbbbfd3c6f0d31269feeea93d0edf383b47
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-01-25 00:03:14 +0300

    Доработка адаптива компонента кнопки

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,16 +1,15 @@
 import { FC, PropsWithChildren } from "react";
 import classes from './Button.module.css'
+
 interface IButtonProps extends PropsWithChildren {
     color?: 'black' | 'white';
 }
 
 const Button: FC<IButtonProps> = ({ children, color = 'black' }) => {
     return (
-        <div>
-            <button className={`${color === 'black' ? classes.black : classes.white} ${classes.default}`}>
-                {children}
-            </button>
-        </div>
+        <button className={`${color === 'black' ? classes.black : classes.white} ${classes.default}`}>
+            {children}
+        </button>
     );
 };
 
