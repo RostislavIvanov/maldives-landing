bb488b8b018a929f2de4f9c8b2b0ba59e0feb1ae
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-01-28 17:33:39 +0300

    Добавление меню бургера

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
-import logo from "../../assets/icons/logo-without-back.svg";
+import logo from "../../assets/icons/logo-white.svg";
 import classes from "./Header.module.css";
 import { FC } from "react";
+import BurgerMenu from "~/components/BurgerMenu/BurgerMenu.tsx";
 
 type HeaderProps = {
-    handleLinkClick: (e: React.MouseEvent<HTMLAnchorElement>, link: string) => void
+    handleLinkClick: (e: React.MouseEvent<HTMLElement>, link: string) => void
 }
 const Header: FC<HeaderProps> = ({ handleLinkClick }) => {
     return (
+        <>
+            <BurgerMenu handleLinkClick={handleLinkClick}/>
             <div className={classes.header}>
                 <nav className={classes.nav}>
                     <ul className={classes.nav__left}>
@@ -23,6 +26,7 @@ const Header: FC<HeaderProps> = ({ handleLinkClick }) => {
                     </ul>
                 </nav>
             </div>
+        </>
     );
 };
 
