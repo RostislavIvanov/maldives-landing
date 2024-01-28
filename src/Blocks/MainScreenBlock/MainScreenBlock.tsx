380b3869dc74b57450d9853eaacb63b0ef27b857
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-01-28 12:13:49 +0300

    Адаптив первого блока

--- src/Blocks/MainScreenBlock/MainScreenBlock.tsx
+++ src/Blocks/MainScreenBlock/MainScreenBlock.tsx
@@ -1,11 +1,11 @@
 import classes from "./MainScreenBlock.module.css";
-import VideoBackground from "../../components/VideoBackground/VideoBackground.tsx";
 import MainScreenContent from "../../components/MainScreenContent/MainScreenContent.tsx";
+import ImageBackground from "~/components/VideoBackground/ImageBackground.tsx";
 
 const MainScreenBlock = () => {
     return (
         <div className={classes.intro}>
-            <VideoBackground/>
+            <ImageBackground/>
             <MainScreenContent/>
         </div>
 
