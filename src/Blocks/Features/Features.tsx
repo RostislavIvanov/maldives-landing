90adccb811cae01421707176930a19520070bce9
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-02-03 20:38:12 +0300

    Рефакторинг

--- src/Blocks/Features/Features.tsx
+++ src/Blocks/Features/Features.tsx
@@ -6,105 +6,56 @@ import heal from '../../assets/images/healing.jpg'
 import resortDay from '../../assets/images/resort-day.jpg'
 import romantic from '../../assets/images/romantic.jpeg'
 import culture from '../../assets/images/culture.jpg'
-import tap from '../../assets/icons/Tap.svg'
+import FeaturesItem from "~/components/FeaturesItem/FeaturesItem.tsx";
+import { FC } from "react";
 
-const Features = () => {
+const Features:FC = () => {
     return (
-            <>
-                <div className={classes.titleWrapper}>
-                    <BlockTitle id={'features'}>Особенности<br/> нашего острова</BlockTitle>
+        <>
+            <div className={classes.titleWrapper}>
+                <BlockTitle id={'features'}>Особенности<br/> нашего острова</BlockTitle>
+            </div>
+            <div className={classes.features}>
+                <div className={classes.features__left}>
+                    <FeaturesItem label={'Свадьбы'}
+                                  subLabel={'Организуем красивую свадебную церемонию на любой вкус и бюджет'}
+                                  imageUrl={wedding} side={"left"}
+                    />
+                    <FeaturesItem label={'Лечение'}
+                                  subLabel={'Совместите приятное с полезным. Предлагаем вам массаж и аюрведические процедуры'}
+                                  imageUrl={heal} side={"left"}
+                    />
                 </div>
-
-                <div className={classes.features}>
-                    <div className={classes.features__left}>
-                        <div className={`${classes.features__picture}`}
-                             style={{ backgroundImage: `url(${wedding})` }} // TODO переделать в tailwind (тут style даже алиасы не принимает)
-                        >
-                            <div className={classes.featuresCard__left}>
-                                <div className={classes.featuresCard}>
-                                    <h2 className={classes.features__label}>Свадьбы</h2>
-                                    <p className={classes.features__subLabel}>Организуем красивую свадебную
-                                        церемонию на любой вкус и бюджет</p>
-                                    <img className={classes.features__icon} src={tap} alt=""/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={classes.features__picture}
-                             style={{ backgroundImage: `url(${heal})` }} // TODO переделать в tailwind (тут style даже алиасы не принимает)
-                        >
-                            <div className={classes.featuresCard__left}>
-                                <div className={classes.featuresCard}>
-                                    <h2 className={classes.features__label}>Лечение</h2>
-                                    <p className={classes.features__subLabel}>Совместите приятное с полезным. Предлагаем
-                                        вам массаж и аюрведические процедуры</p>
-                                    <img className={classes.features__icon} src={tap} alt=""/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={classes.features__right}>
-                        <div className={classes.features__picture}
-                             style={{ backgroundImage: `url(${cruise})` }} // TODO переделать в tailwind (тут style даже алиасы не принимает)
-                        >
-                            <div className={classes.featuresCard__right}>
-                                <div className={classes.featuresCard}>
-                                    <h2 className={classes.features__label}>Круизы</h2>
-                                    <p className={classes.features__subLabel}>Арендуйте яхту с друзьями или семьей и
-                                        отправьтесь в путешествие по своему уникальному маршруту</p>
-                                    <img className={classes.features__icon} src={tap} alt=""/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className={classes.features__right}>
+                    <FeaturesItem label={'Круизы'}
+                                  subLabel={'Арендуйте яхту с друзьями или семьей и отправьтесь в путешествие по своему ' +
+                                      'уникальному маршруту'}
+                                  imageUrl={cruise} side={"right"}
+                    />
                 </div>
-
-                <div className={classes.features}>
-                    <div className={classes.features__right}>
-                        <div className={classes.features__picture}
-                             style={{ backgroundImage: `url(${resortDay})` }} // TODO переделать в tailwind (тут style даже алиасы не принимает)
-                        >
-                            <div className={classes.featuresCard__right}>
-                                <div className={classes.featuresCard}>
-                                    <h2 className={classes.features__label}>День на резорте</h2>
-                                    <p className={classes.features__subLabel}>Мы отвезем вас в один из отелей-резортов,
-                                        где вы сможете насладиться беззаботным отдыхом в райском месте</p>
-                                    <img className={classes.features__icon} src={tap} alt=""/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={classes.features__left}>
-                        <div className={classes.features__picture}
-                             style={{ backgroundImage: `url(${romantic})` }} // TODO переделать в tailwind (тут style даже алиасы не принимает)
-                        >
-                            <div className={classes.featuresCard__left}>
-                                <div className={classes.featuresCard}>
-                                    <h2 className={classes.features__label}>Романтический ужин</h2>
-                                    <p className={classes.features__subLabel}>Мы поможем красиво отметить любое ваше
-                                        событие</p>
-                                    <img className={classes.features__icon} src={tap} alt=""/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={classes.features__picture}
-                             style={{ backgroundImage: `url(${culture})` }} // TODO переделать в tailwind (тут style даже алиасы не принимает)
-                        >
-                            <div className={classes.featuresCard__left}>
-                                <div className={classes.featuresCard}>
-                                    <h2 className={classes.features__label}>Культура Мальдив</h2>
-                                    <p className={classes.features__subLabel}>Мы сможем скрасить ваше пребывание на
-                                        нашем
-                                        острове национальными танцами, песнями, организовать мастер-классы по
-                                        изготовлению мальдивского сувенира, а так же фотосессию в национальных
-                                        костюмах.</p>
-                                    <img className={classes.features__icon} src={tap} alt=""/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            </div>
+            <div className={classes.features}>
+                <div className={classes.features__right}>
+                    <FeaturesItem label={'День на резорте'}
+                                  subLabel={'Мы отвезем вас в один из отелей-резортов, где вы сможете насладиться ' +
+                                      'беззаботным отдыхом в райском месте'}
+                                  imageUrl={resortDay} side={"right"}
+                    />
                 </div>
-            </>
+                <div className={classes.features__left}>
+                    <FeaturesItem label={'Романтический ужин'}
+                                  subLabel={'Мы поможем красиво отметить любое ваше событие'}
+                                  imageUrl={romantic} side={"left"}
+                    />
+                    <FeaturesItem label={'Культура Мальдив'}
+                                  subLabel={'Мы сможем скрасить ваше пребывание на нашем острове национальными танцами, ' +
+                                      'песнями, организовать мастер-классы по изготовлению мальдивского сувенира, а так ' +
+                                      'же фотосессию в национальных костюмах.'}
+                                  imageUrl={culture} side={"left"}
+                    />
+                </div>
+            </div>
+        </>
     );
 };
 
---
+++ src/components/FeaturesItem/FeaturesItem.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import classes from "~/Blocks/Features/Features.module.css";
+import tap from "~/assets/icons/Tap.svg";
+
+type FeaturesItemProps = {
+    label: string;
+    subLabel: string;
+    imageUrl: string;
+    side: "left" | "right";
+}
+const FeaturesItem: FC<FeaturesItemProps> = ({ label, subLabel, imageUrl, side }) => {
+    return (
+        <div className={classes.features__picture}
+             style={{ backgroundImage: `url(${imageUrl})` }} // TODO переделать в tailwind (тут style даже алиасы не принимает)
+        >
+            <div className={side === 'right' ? classes.featuresCard__right : classes.featuresCard__left}>
+                <div className={classes.featuresCard}>
+                    <h2 className={classes.features__label}>{label}</h2>
+                    <p className={classes.features__subLabel}>{subLabel}</p>
+                    <img className={classes.features__icon} src={tap} alt=""/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturesItem;
