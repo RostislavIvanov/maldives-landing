25dfe836e971c7ad47938e058bc442210343ee51
Svyatoslav Semenov <semenovslava293@gmail.com>
2024-02-04 23:17:47 +0300

    Вынос SliderContext в отдельный файл

--- src/components/Arrows/Arrows.tsx
+++ src/components/Arrows/Arrows.tsx
@@ -1,7 +1,7 @@
 import { FC, useContext } from "react";
 import blackArrow from "../../assets/icons/black-arrow.svg";
-import { SliderContext } from "~/components/Slider/Slider.tsx";
 import classes from "./Arrows.module.css";
+import SliderContext from "~/utils/SliderContext/SliderContext.ts";
 
 const Arrows: FC = () => {
     const { changeSlide } = useContext(SliderContext);
