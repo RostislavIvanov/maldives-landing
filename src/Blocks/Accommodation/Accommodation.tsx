4065d1a1684689c50b7388567203c2953972e40e
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-02-13 11:22:17 +0300

    Доработка блока размещения

--- src/Blocks/Accommodation/Accommodation.tsx
+++ src/Blocks/Accommodation/Accommodation.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
 import classes from './Accommodation.module.css'
-import rosyVilla from '~/assets/images/232311885.jpg'
+import rosyVilla from '~/assets/images/photo_2023-12-28_10-10-53 (1).jpg'
 import rosyVilla1 from '~/assets/images/232863959 (1).jpg'
 import rosyVilla2 from '~/assets/images/271603025.jpg'
 import star from '~/assets/icons/star.svg'
-// import arrow from '~/assets/icons/arrow-right-black.svg'
+import arrow from '~/assets/icons/arrow-right-black.svg'
 import Button from "~/components/UI/Button/Button.tsx";
+import { useNavigate } from "react-router-dom";
 
 const Accommodation: FC = () => {
+    const navigate = useNavigate();
+    const handleLink = () => {
+        navigate('/rosyVillaHotel')
+    }
     return (
         <div className={classes.content}>
             <div className={classes.wrapper}>
@@ -27,21 +32,30 @@ const Accommodation: FC = () => {
                                     <img src={star} alt=""/>
                                     <img src={star} alt=""/>
                                 </div>
+                                <div className={classes.accommodation__grade}>
+                                    <span>8.7</span> — средняя оценка среди отзывов на Букинге
+                                </div>
                                 <p>
                                     Rosy Villa Hotel — это отель, расположенный в городе Гурайдо. К услугам гостей
                                     бесплатные велосипеды, общий лаундж, терраса и ресторан. Вариант размещения
                                     расположен в 100м от такой достопримечательности, как Пляж Гурайду.
                                 </p>
                                 <div className={classes.accommodation__btn}>
-                                    <Button>Подробнее</Button>
+                                    <Button onClick={handleLink}>Подробнее</Button>
                                 </div>
                             </div>
                             <div className={classes.accommodation__images}>
                                 <div className={classes.accommodation__imagesItemCon}>
                                     <img className={classes.accommodation__imagesItem} src={rosyVilla1} alt=""/>
+                                    <div onClick={handleLink} className={classes.accommodation__arrow}>
+                                        <img src={arrow} alt="->"/>
+                                    </div>
                                 </div>
                                 <div className={classes.accommodation__imagesItemCon}>
                                     <img className={classes.accommodation__imagesItem} src={rosyVilla2} alt=""/>
+                                    <div onClick={handleLink} className={classes.accommodation__arrow}>
+                                        <img src={arrow} alt="->"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
