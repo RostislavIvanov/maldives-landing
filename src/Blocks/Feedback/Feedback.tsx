8ab4eeadf29fd7ccfcbde877444b282d6f18d31c
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-02-22 20:12:48 +0300

    esLint

--- src/Blocks/Feedback/Feedback.tsx
+++ src/Blocks/Feedback/Feedback.tsx
@@ -6,13 +6,15 @@ import fb2 from '~/assets/images/feedback-2.jpg';
 import fb3 from '~/assets/images/feedback-3.jpg';
 import fb4 from '~/assets/images/feedback-4.jpg';
 import fb5 from '~/assets/images/feedback-5.jpg';
+
 const Feedback = () => {
     return (
         <div className={classes.content}>
             <div className={classes.wrapper}>
                 <BlockTitle>Отзывы</BlockTitle>
                 <div className={classes.sliderCon}>
-                    <Slider objectFitStyle={'contain'} autoPlay={true} autoPlayTime={8000} images={[fb1, fb2, fb3, fb4, fb5]}/>
+                    <Slider objectFitStyle={'contain'} autoPlay={true} autoPlayTime={8000}
+                            images={[ fb1, fb2, fb3, fb4, fb5 ]}/>
                 </div>
             </div>
         </div>
