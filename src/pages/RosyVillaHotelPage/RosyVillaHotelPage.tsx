a950aa33e0035bede304ca77a3b7e3ca9c580ecd
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-02-22 21:36:55 +0300

    Добавление блока FAQ

--- src/pages/RosyVillaHotelPage/RosyVillaHotelPage.tsx
+++ src/pages/RosyVillaHotelPage/RosyVillaHotelPage.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
 import HotelRooms from '~/Blocks/HotelRooms/HotelRooms.tsx';
 import Feedback from '~/Blocks/Feedback/Feedback.tsx';
+import FAQ from '~/Blocks/FAQ/FAQ.tsx';
+import { faqData } from '~/data/faqData.ts';
 
 const RosyVillaHotelPage: FC = () => {
     return (
         <div>
             <HotelRooms/>
             <Feedback/>
+            <FAQ data={faqData}/>
         </div>
     );
 };
