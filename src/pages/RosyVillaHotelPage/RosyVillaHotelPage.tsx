40f23549f0a80b42e38db2933ba79f49016877e3
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-02-22 20:11:48 +0300

    Добавление блока Отзывов

--- src/pages/RosyVillaHotelPage/RosyVillaHotelPage.tsx
+++ src/pages/RosyVillaHotelPage/RosyVillaHotelPage.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import HotelRooms from '~/Blocks/HotelRooms/HotelRooms.tsx';
+import Feedback from '~/Blocks/Feedback/Feedback.tsx';
 
 const RosyVillaHotelPage: FC = () => {
     return (
         <div>
             <HotelRooms/>
+            <Feedback/>
         </div>
     );
 };
