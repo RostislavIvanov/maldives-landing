b123a2c6878757acba12bcc60b1e106ef519eb54
Rostislav Ivanov <rostislav.ivanov@bscmsc.ru>
2024-02-24 23:19:03 +0300

    Исправления по блоку FAQ

--- src/Blocks/FAQ/FAQ.tsx
+++ src/Blocks/FAQ/FAQ.tsx
@@ -22,33 +22,30 @@ const FAQ: FC<FAQProps> = ({ data }) => {
                 <div className={classes.titleWrapper}>
                     <BlockTitle>Часто задаваемые вопросы</BlockTitle>
                 </div>
-                <div className="">
-                    {data.map((faq, index) => (
-                        <div key={index}>
-                            <div className={classes.questionItem} onClick={() => toggleFAQ(index)}>
-                                <div className={classes.questionItem__con}>
-                                    <h5 className={classes.questionItem__title}>{faq.question}</h5>
-                                    <svg
-                                        className={`transition-transform transform ${isOpen === index ? 'rotate-180' : ''} w-[40px] h-[40px]`}
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path d="M19 9l-7 7-7-7"></path>
-                                    </svg>
-                                </div>
-                                {isOpen === index && (
-                                    <p className={classes.questionItem__answer}>{faq.answer}</p>
-                                )}
+                {data.map((faq, index) => (
+                    <div key={index}>
+                        <div className={classes.questionItem} onClick={() => toggleFAQ(index)}>
+                            <div className={classes.questionItem__con}>
+                                <h5 className={classes.questionItem__title}>{faq.question}</h5>
+                                <svg
+                                    className={`transition-transform transform ${isOpen === index ? 'rotate-180' : ''} w-[40px] h-[40px]`}
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M19 9l-7 7-7-7"></path>
+                                </svg>
                             </div>
+                            {isOpen === index && (
+                                <p className={classes.questionItem__answer}>{faq.answer}</p>
+                            )}
                         </div>
-
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
