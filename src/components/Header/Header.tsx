36e4ffb72c0ab7f2de7ae1063a604a385400b63e
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-17 22:22:24 +0300

    Перевод главного блока

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,14 @@ import logo from '~/assets/icons/logo-white.svg';
 import classes from './Header.module.css';
 import { FC } from 'react';
 import BurgerMenu from '~/components/BurgerMenu/BurgerMenu.tsx';
-import { useTranslation } from 'react-i18next';
 import LangSwitch from '~/components/UI/LangSwitch/LangSwitch.tsx';
+import { useTranslate } from '~/hooks/useTranslate/useTranslate.ts';
 
 type HeaderProps = {
     handleLinkClick: (e: React.MouseEvent<HTMLElement>, link: string) => void
 }
 const Header: FC<HeaderProps> = ({ handleLinkClick }) => {
-    const { t } = useTranslation('global');
+    const t = useTranslate('header');
     return (
         <>
             <BurgerMenu handleLinkClick={handleLinkClick}/>
@@ -17,13 +17,13 @@ const Header: FC<HeaderProps> = ({ handleLinkClick }) => {
                 <nav className={classes.nav}>
                     <ul className={classes.nav__left}>
                         <li><a href="#about" onClick={e => handleLinkClick(e, 'about')}>
-                            {t('header.nav.about')}
+                            {t('nav.about')}
                         </a></li>
                         <li><a href="#excursions" onClick={e => handleLinkClick(e, 'excursions')}>
-                            {t('header.nav.excursions')}
+                            {t('nav.excursions')}
                         </a></li>
                         <li><a href="#accommodation" onClick={e => handleLinkClick(e, 'accommodation')}>
-                            {t('header.nav.accommodation')}
+                            {t('nav.accommodation')}
                         </a></li>
                     </ul>
                     <div className={classes.nav__logo}>
@@ -31,10 +31,10 @@ const Header: FC<HeaderProps> = ({ handleLinkClick }) => {
                     </div>
                     <ul className={classes.nav__right}>
                         <li><a href="#features" onClick={e => handleLinkClick(e, 'features')}>
-                            {t('header.nav.features')}
+                            {t('nav.features')}
                         </a></li>
                         <li><a href="#contacts" onClick={e => handleLinkClick(e, 'contacts')}>
-                            {t('header.nav.contacts')}
+                            {t('nav.contacts')}
                         </a></li>
                         <li className={classes.nav__switch}>
                             <LangSwitch/>
