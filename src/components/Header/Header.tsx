e769d0057578bdd78f8f40bfc52bd1a6be072080
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-17 15:24:35 +0300

    Добавление переключателя и начало перевода Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,27 +2,43 @@ import logo from '~/assets/icons/logo-white.svg';
 import classes from './Header.module.css';
 import { FC } from 'react';
 import BurgerMenu from '~/components/BurgerMenu/BurgerMenu.tsx';
+import { useTranslation } from 'react-i18next';
+import LangSwitch from '~/components/UI/LangSwitch/LangSwitch.tsx';
 
 type HeaderProps = {
     handleLinkClick: (e: React.MouseEvent<HTMLElement>, link: string) => void
 }
 const Header: FC<HeaderProps> = ({ handleLinkClick }) => {
+    const { t } = useTranslation('global');
     return (
         <>
             <BurgerMenu handleLinkClick={handleLinkClick}/>
             <div className={classes.header}>
                 <nav className={classes.nav}>
                     <ul className={classes.nav__left}>
-                        <li><a href='#about' onClick={e => handleLinkClick(e, 'about')}>Об острове</a></li>
-                        <li><a href='#excursions' onClick={e => handleLinkClick(e, 'excursions')}>Экскурсии</a></li>
-                        <li><a href='#accommodation' onClick={e => handleLinkClick(e, 'accommodation')}>Размещение</a></li>
+                        <li><a href="#about" onClick={e => handleLinkClick(e, 'about')}>
+                            {t('header.nav.about')}
+                        </a></li>
+                        <li><a href="#excursions" onClick={e => handleLinkClick(e, 'excursions')}>
+                            {t('header.nav.excursions')}
+                        </a></li>
+                        <li><a href="#accommodation" onClick={e => handleLinkClick(e, 'accommodation')}>
+                            {t('header.nav.accommodation')}
+                        </a></li>
                     </ul>
                     <div className={classes.nav__logo}>
-                        <img src={logo} alt='logo'/>
+                        <img src={logo} alt="logo"/>
                     </div>
                     <ul className={classes.nav__right}>
-                        <li><a href='#features' onClick={e => handleLinkClick(e, 'features')}>Наши программы</a></li>
-                        <li><a href='#prices' onClick={e => handleLinkClick(e, 'prices')}>Цены и контакты</a></li>
+                        <li><a href="#features" onClick={e => handleLinkClick(e, 'features')}>
+                            {t('header.nav.features')}
+                        </a></li>
+                        <li><a href="#contacts" onClick={e => handleLinkClick(e, 'contacts')}>
+                            {t('header.nav.contacts')}
+                        </a></li>
+                        <li className={classes.nav__switch}>
+                            <LangSwitch/>
+                        </li>
                     </ul>
                 </nav>
             </div>
