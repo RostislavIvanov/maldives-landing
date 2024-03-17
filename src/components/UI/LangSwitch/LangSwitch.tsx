31c077fb14464425edab33cf02440fd38c52319e
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-17 15:24:07 +0300

    Создание компонента переклчателя языка

---
+++ src/components/UI/LangSwitch/LangSwitch.tsx
@@ -0,0 +1,29 @@
+import { FC, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import classes from './LangSwitch.module.css';
+
+const LangSwitch: FC = () => {
+    const [ toggleLang, setToggleLang ] = useState('ru');
+    const { i18n } = useTranslation('global');
+
+    useEffect(() => {
+        i18n.changeLanguage(toggleLang);
+    }, [toggleLang]);
+
+    const handleToggleLanguage = () => {
+        setToggleLang(prevState => prevState === 'ru' ? 'en' : 'ru');
+    };
+
+    return (
+        <div className={classes.switch} onClick={handleToggleLanguage}>
+            <div className={`${classes.switch__langRu} ${toggleLang === 'ru' ? classes.switch__chosen : ''}`}>
+                Ru
+            </div>
+            <div className={`${classes.switch__langEn} ${toggleLang === 'en' ? classes.switch__chosen : ''}`}>
+                En
+            </div>
+        </div>
+    );
+};
+
+export default LangSwitch;
