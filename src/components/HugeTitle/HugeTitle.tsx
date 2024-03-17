763aa7f4a0bfd5bba7063b2224b6e44d7736f947
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-17 22:22:10 +0300

    Перевод главного блока

--- src/components/HugeTitle/HugeTitle.tsx
+++ src/components/HugeTitle/HugeTitle.tsx
@@ -1,22 +1,24 @@
 import classes from './HugeTitle.module.css';
 import Button from '~/components/UI/Button/Button.tsx';
 import { FC } from 'react';
+import { useTranslate } from '~/hooks/useTranslate/useTranslate.ts';
 
 type HugeTitleProps = {
     handleLinkClick: (e: React.MouseEvent<HTMLElement>, link: string) => void;
 }
 
 const HugeTitle: FC<HugeTitleProps> = ({ handleLinkClick }) => {
+    const t = useTranslate('header');
     return (
         <div>
-            <h1 className={classes.huge__title}>Мальдивы</h1>
-            <div className={classes.huge__subtitle}>Ваше незабываемое приключение</div>
+            <h1 className={classes.huge__title}>{t('main.title')}</h1>
+            <div className={classes.huge__subtitle}>{t('main.subTitle')}</div>
 
             <div className={classes.huge__stripe}>|</div>
 
             <div className={classes.huge__btn}>
-                <Button onClick={e => handleLinkClick(e, 'prices')} color={'white'}>
-                    Подобрать тур
+                <Button onClick={e => handleLinkClick(e, 'contacts')} color={'white'}>
+                    {t('main.button')}
                 </Button>
             </div>
         </div>
