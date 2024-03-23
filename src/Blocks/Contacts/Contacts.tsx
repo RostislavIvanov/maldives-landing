2d9f50f90943c42c53f6496b673bd443bfecfe2c
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-23 20:30:09 +0300

    Перевод блока Контакты

--- src/Blocks/Contacts/Contacts.tsx
+++ src/Blocks/Contacts/Contacts.tsx
@@ -6,14 +6,17 @@ import logo from '~/assets/images/logo-original.png';
 import whatsApp from '~/assets/icons/WhatsApp.svg';
 import viber from '~/assets/icons/Viber.svg';
 import telegram from '~/assets/icons/Telegram.svg';
+import { useTranslate } from '~/hooks/useTranslate/useTranslate.ts';
 
 const Contacts: FC = () => {
+    const t = useTranslate('contacts');
+
     return (
         <div className={classes.content}>
             <div className={classes.wrapper}>
                 <div className={classes.titleWrapper}>
                     <BlockTitle id={'contacts'}>
-                        Контакты
+                        {t('title')}
                     </BlockTitle>
                 </div>
                 <div className={classes.contacts}>
@@ -22,14 +25,16 @@ const Contacts: FC = () => {
                     </div>
                     <div className={classes.contacts__column}>
                         <h6 className={classes.contacts__title}>
-                            ROSY VILLA PVT LTD
+                            {t('en.header')}
                         </h6>
                         <p className={classes.contacts__section}>
-                            Для консультаций и бронирования на английском языке
+                            {t('en.consulting')}
                         </p>
 
                         <div className={classes.contacts__item}>
-                            <div className={classes.contacts__label}>Почта:</div>
+                            <div className={classes.contacts__label}>
+                                {t('mail')}
+                            </div>
                             <div className={classes.contacts__link}>
                                 <div>reservations@rosyvillamaldives.com</div>
                                 <div>Rosyc19mid@gmail.com</div>
@@ -37,7 +42,9 @@ const Contacts: FC = () => {
                         </div>
 
                         <div className={classes.contacts__item}>
-                            <div className={classes.contacts__label}>Телефон:</div>
+                            <div className={classes.contacts__label}>
+                                {t('tel')}
+                            </div>
                             <div className={classes.contacts__link}>
                                 <a href="tel:+960787001">+960 999 4740</a>
                             </div>
@@ -57,29 +64,39 @@ const Contacts: FC = () => {
                     </div>
 
                     <div className={classes.contacts__column}>
-                        <h6 className={classes.contacts__title}>Наши партнеры</h6>
+                        <h6 className={classes.contacts__title}>
+                            {t('ru.header')}
+                        </h6>
                         <p className={classes.contacts__section}>
-                            Для консультаций и бронирования на русском языке
+                            {t('ru.consulting')}
                         </p>
 
                         <div className={classes.contacts__item}>
-                            <div className={classes.contacts__label}>Почта:</div>
+                            <div className={classes.contacts__label}>
+                                {t('mail')}
+                            </div>
                             <div className={classes.contacts__link}>
                                 <div>veter-peremen-tur@yandex.ru</div>
                             </div>
                         </div>
 
                         <div className={classes.contacts__item}>
-                            <div className={classes.contacts__label}>Телефон:</div>
+                            <div className={classes.contacts__label}>
+                                {t('tel')}
+                            </div>
                             <div className={classes.contacts__link}>
                                 <a href="tel:+79858982743">+7(985)898-27-43</a>
                             </div>
                         </div>
 
                         <div className={classes.contacts__item}>
-                            <div className={classes.contacts__label}>Сайт:</div>
+                            <div className={classes.contacts__label}>
+                                {t('site')}
+                            </div>
                             <div className={classes.contacts__link}>
-                                <a target="_blank" href="https://tkveterperemen.ru/">ООО ТК «Ветер Перемен» tkveterperemen.ru</a>
+                                <a target="_blank" href="https://tkveterperemen.ru/">
+                                    https://tkveterperemen.ru
+                                </a>
                             </div>
                         </div>
 
@@ -99,9 +116,7 @@ const Contacts: FC = () => {
                 <hr/>
                 <div className={classes.contacts__desc}>
                     <Description>
-                        Мы предлагаем присоединиться к уже готовой программе или можем просчитать индивидуальный маршрут
-                        для ваших задач. Для консультации, расчета и бронирования на русском языке обращайтесь к нашим
-                        партнерам в России.
+                        {t('description')}
                     </Description>
                 </div>
             </div>
