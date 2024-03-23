4bfadcfd316158c0c1042a8154baf108cd4cfe5f
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-23 23:08:42 +0300

    Перевод блока Отзывов

--- src/Blocks/Feedback/Feedback.tsx
+++ src/Blocks/Feedback/Feedback.tsx
@@ -6,12 +6,17 @@ import fb2 from '~/assets/images/feedback-2.jpg';
 import fb3 from '~/assets/images/feedback-3.jpg';
 import fb4 from '~/assets/images/feedback-4.jpg';
 import fb5 from '~/assets/images/feedback-5.jpg';
+import { useTranslate } from '~/hooks/useTranslate/useTranslate.ts';
 
 const Feedback = () => {
+    const t = useTranslate('feedback');
+
     return (
         <div className={classes.content}>
             <div className={classes.wrapper}>
-                <BlockTitle>Отзывы</BlockTitle>
+                <BlockTitle>
+                    {t('title')}
+                </BlockTitle>
                 <div className={classes.sliderCon}>
                     <Slider objectFitStyle={'contain'} autoPlay={true} autoPlayTime={8000}
                             images={[ fb1, fb2, fb3, fb4, fb5 ]}/>
