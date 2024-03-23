94738f904e51a5bffe9180c14469ca2daf9688d4
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-23 21:19:07 +0300

    Вынос данных о модальных фото блока Номеров

---
+++ src/data/hotelRoomsImageData.ts
@@ -0,0 +1,26 @@
+import standard from '~/assets/images/standard.jpg';
+import standard1 from '~/assets/images/standard-1.jpg';
+import standard2 from '~/assets/images/standard-2.jpg';
+import standard3 from '~/assets/images/standard-3.jpg';
+import standard4 from '~/assets/images/standard-4.jpg';
+import standard5 from '~/assets/images/standard-5.jpg';
+import standard6 from '~/assets/images/standard-6.jpg';
+import familyDeluxe1 from '~/assets/images/family-deluxe-1.jpg';
+import deluxe from '~/assets/images/deluxe.jpg';
+import deluxe1 from '~/assets/images/deluxe-1.jpg';
+import deluxe2 from '~/assets/images/deluxe-2.jpg';
+import deluxe3 from '~/assets/images/deluxe-3.jpg';
+import familyDeluxe from '~/assets/images/family-deluxe.jpg';
+import familyDeluxe2 from '~/assets/images/family-deluxe-2.jpg';
+import familyDeluxe3 from '~/assets/images/family-deluxe-3.jpg';
+import premium from '~/assets/images/premium.jpg';
+import premium3 from '~/assets/images/premium-3.jpg';
+import premium4 from '~/assets/images/premium-4.jpg';
+
+export const hotelRoomsImageData = [
+    [ standard, standard1, standard2, standard3, standard4, standard5, standard6, familyDeluxe1 ],
+    [ deluxe, deluxe1, deluxe2, standard3, standard4, deluxe3, familyDeluxe1 ],
+    [ familyDeluxe, familyDeluxe2, familyDeluxe3, standard3, standard4, familyDeluxe1 ],
+    [ premium, premium3, standard4, premium4 ]
+];
+
