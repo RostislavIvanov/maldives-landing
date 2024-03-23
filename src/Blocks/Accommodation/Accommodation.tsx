67698c422ef30f5f6603a79648c5375db2933981
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-23 19:35:33 +0300

    Перевод блока Размещения

--- src/Blocks/Accommodation/Accommodation.tsx
+++ src/Blocks/Accommodation/Accommodation.tsx
@@ -7,12 +7,15 @@ import star from '~/assets/icons/star.svg';
 import arrow from '~/assets/icons/arrow-right-black.svg';
 import Button from '~/components/UI/Button/Button.tsx';
 import { useNavigate } from 'react-router-dom';
+import { useTranslate } from '~/hooks/useTranslate/useTranslate.ts';
 
 const Accommodation: FC = () => {
     const navigate = useNavigate();
+    const t = useTranslate('accommodation');
     const handleLink = () => {
         navigate('/rosyVillaHotel');
     };
+
     return (
         <div className={classes.content}>
             <div className={classes.wrapper}>
@@ -25,7 +28,9 @@ const Accommodation: FC = () => {
                     <div className={classes.accommodation__item}>
                         <div className={classes.accommodation__info}>
                             <div className={classes.accommodation__text}>
-                                <h2 id={'accommodation'} className={classes.accommodation__hotelName}>Rosy Villa Hotel</h2>
+                                <h2 id={'accommodation'} className={classes.accommodation__hotelName}>
+                                    Rosy Villa Hotel
+                                </h2>
                                 <div className={classes.accommodation__stars}>
                                     <img src={star} alt=""/>
                                     <img src={star} alt=""/>
@@ -33,18 +38,11 @@ const Accommodation: FC = () => {
                                     <img src={star} alt=""/>
                                 </div>
                                 <div className={classes.accommodation__grade}>
-                                    <span>8.7</span> — средняя оценка среди отзывов на Букинге
+                                    <span>8.7</span>{t('grade')}
                                 </div>
-                                <p>
-                                    Отель Rosy Villa Hotel расположен на локальном острове К. Гурайду в Южном
-                                    Мале Атолле. Мы доставляем гостей до острова на собственном катере. Это единственный
-                                    отель на острове с
-                                    бассейном, а на крыше есть открытая терраса.<br/> В 5-ти минутах ходьбы есть пляж.
-                                    Вы можете взять в отеле ласты и маску для снорклинга,
-                                    а так же заказать различные экскурсии.
-                                </p>
+                                <p dangerouslySetInnerHTML={{ __html: t('info') }}/>
                                 <div className={classes.accommodation__btn}>
-                                    <Button onClick={handleLink}>Подробнее</Button>
+                                    <Button onClick={handleLink}>{t('button')}</Button>
                                 </div>
                             </div>
                             <div className={classes.accommodation__images}>
