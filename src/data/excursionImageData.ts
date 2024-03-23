e41dec27c0a12518ee8481c2ac7f54701af1cb45
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-23 16:07:41 +0300

    Вынесение данных о фото экскурсий

---
+++ src/data/excursionImageData.ts
@@ -0,0 +1,50 @@
+import points from '~/assets/images/points.jpg';
+import points1 from '~/assets/images/points-1.jpg';
+import points2 from '~/assets/images/points-2.jpg';
+import points3 from '~/assets/images/points-3.jpg';
+import points4 from '~/assets/images/points-4.jpg';
+import points5 from '~/assets/images/points-5.jpg';
+import fishing from '~/assets/images/fishing.jpg';
+import fishing1 from '~/assets/images/fishing-1.jpg';
+import fishing2 from '~/assets/images/fishing-2.jpg';
+import fishing3 from '~/assets/images/fishing-3.jpg';
+import fishing4 from '~/assets/images/fishing-4.jpg';
+import fishing5 from '~/assets/images/fishing-5.jpg';
+import fishing6 from '~/assets/images/fishing-6.jpg';
+import dolphin from '~/assets/images/dolphine-tour.jpg';
+import dolphin1 from '~/assets/images/dolphine-tour-1.jpg';
+import shark from '~/assets/images/sharks.jpg';
+import shark1 from '~/assets/images/shark-1.jpg';
+import shark2 from '~/assets/images/shark-2.jpg';
+import shark3 from '~/assets/images/shark-3.jpg';
+import shark4 from '~/assets/images/shark-4.jpg';
+import ship from '~/assets/images/Ship.jpg';
+import ship1 from '~/assets/images/ship-1.jpg';
+import ship2 from '~/assets/images/ship-2.jpg';
+import ship3 from '~/assets/images/ship-3.jpg';
+import ship4 from '~/assets/images/ship-4.jpg';
+import whaleShark from '~/assets/images/whale-shark.jpg';
+import whaleShark1 from '~/assets/images/whale-shark-1.jpg';
+import whaleShark2 from '~/assets/images/whale-shark-2.jpg';
+import island from '~/assets/images/island.jpg';
+import dinner from '~/assets/images/dinner.jpg';
+import dinner1 from '~/assets/images/dinner-1.jpg';
+import dinner2 from '~/assets/images/dinner-2.jpg';
+import dinner4 from '~/assets/images/dinner-4.jpg';
+import snorkeling from '~/assets/images/snorkeling.jpg';
+import snorkeling1 from '~/assets/images/snorkeling-1.jpg';
+import snorkeling2 from '~/assets/images/snorkeling-2.jpg';
+import snorkeling3 from '~/assets/images/snorkeling-3.jpg';
+import snorkeling4 from '~/assets/images/snorkeling-4.jpg';
+
+export const excursionImageData = [
+    [ points, points1, points2, points3, points4, points5 ],
+    [ fishing, fishing1, fishing2, fishing3, fishing4, fishing5, fishing6 ],
+    [ dolphin, dolphin1 ],
+    [ shark, shark1, shark2, shark3, shark4 ],
+    [ ship, ship1, ship2, ship3, ship4 ],
+    [ whaleShark, whaleShark1, whaleShark2 ],
+    [ island ],
+    [ dinner, dinner1, dinner2, dinner4 ],
+    [ snorkeling, snorkeling1, snorkeling2, snorkeling3, snorkeling4 ]
+];
