d317ef2643bb9bc2fa98c1493aef7a376a93c9e2
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-23 19:05:52 +0300

    Перевод блока Программ

--- src/Blocks/Programs/Programs.tsx
+++ src/Blocks/Programs/Programs.tsx
@@ -11,91 +11,82 @@ import ProgramsItem from '~/components/ProgramsItem/ProgramsItem.tsx';
 import { FC } from 'react';
 import { useModal } from '~/hooks/useModal/useModal.tsx';
 import Description from '~/components/Description/Description.tsx';
+import { useTranslate } from '~/hooks/useTranslate/useTranslate.ts';
 
 const Programs: FC = () => {
-    const [
-        ModalPanel,
-        openModal,
-    ] = useModal(4000);
+    const [ ModalPanel, openModal ] = useModal(4000);
+    const t = useTranslate('programs');
 
     return (
         <>
             <div className={classes.titleWrapper}>
-                <BlockTitle
-                    label={'Мы регулярно набираем группы с тематическими программами: йога и фото туры, ' +
-                        'литературный тур, приключенческий с изучением английского языка,  туры со звездами, медитативные ' +
-                        'и психологические туры. Актуальные предложения узнавайте у наших менеджеров. Может быть планируете свадьбу? Или хотите восстановить здоровье? А возможно вы участник дайверского\n' +
-                        '                    или спортивного клуба? Мы можем организовать под ваше мероприятие индивидуальную программу.'}
-                    id={'features'}
-                >
-                    Наши программы
+                <BlockTitle label={t('subTitle')} id={'features'}>
+                    {t('title')}
                 </BlockTitle>
             </div>
             <div className={classes.features}>
                 <div className={classes.features__left}>
                     <ProgramsItem
-                        label={'Свадьбы'}
-                        subLabel={'Организуем красивую свадебную церемонию на любой вкус и бюджет'}
+                        label={t(`items.${0}.label`)}
+                        subLabel={t(`items.${0}.subLabel`)}
                         imageUrl={wedding} side={'left'}
                         openModal={openModal}
                         modalImages={[ wedding ]}
-                        modalText={'<p>Организуем красивую свадебную церемонию с фотосессией. На любой вкус и кошелек. Ее можно сделать как на нашем локальном острове на пляже, так и на острове - резорте и даже на необитаемом острове. Белый свадебный наряд и белая фата особенно красиво смотрится на белом песке, на фоне бирюзового моря и такой красивый ритуал оставит самые яркие воспоминания и фотографии счастливых моментов. Юридической силы эта церемония не имеет, но послужит красивым началом счастливой семейной жизни!</p>'}
+                        modalText={t(`items.${0}.modalText`)}
                     />
                     <ProgramsItem
-                        label={'Лечение'}
-                        subLabel={'Совместите приятное с полезным. Предлагаем вам массаж и аюрведические процедуры'}
+                        label={t(`items.${1}.label`)}
+                        subLabel={t(`items.${1}.subLabel`)}
                         imageUrl={heal} side={'left'}
                         openModal={openModal}
                         modalImages={[ heal ]}
-                        modalText={'<p>Предлагаем совместить и приятное и полезное. Вы можете заказать аюрведическую программу лечения, курс массажа или косметологические уходы. Многие гости Мальдив отмечают, что избавились от неприятных недугов, таких как аллергия, кисты, остеохондроз и даже рак. Климат, яркие цвета, состав воды, солнце оказывают благотворное влияние на организм. Мы предлагаем усилить это полезное влияние дополнительными оздоровительными процедурами.</p>'}
+                        modalText={t(`items.${1}.modalText`)}
                     />
                 </div>
                 <div className={classes.features__right}>
                     <ProgramsItem
-                        label={'Круизы'}
-                        subLabel={'Арендуйте яхту с друзьями или семьей и отправьтесь в путешествие по своему ' +
-                            'уникальному маршруту'}
+                        label={t(`items.${2}.label`)}
+                        subLabel={t(`items.${2}.subLabel`)}
                         imageUrl={cruise} side={'right'}
                         openModal={openModal}
                         modalImages={[ cruise ]}
-                        modalText={'<p>Вы можете арендовать яхту для вашей группы или компании и отправиться в круиз по вашему маршруту либо по готовым вариантам. Питанием, командой, оборудованием для снорклинга обеспечиваем. Каждый вечер можем организовать дискотеку с диджеем и баром, танцы под луной посреди океана. И пусть вам позавидуют звезды!</p><p>Стоимость аренды рассчитывается индивидуально, в зависимости от поставленных задач. За расчетом обратитесь к менеджеру по указанным на сайте телефонам.</p>'}
+                        modalText={t(`items.${2}.modalText`)}
                     />
                 </div>
             </div>
             <div className={classes.features}>
                 <div className={classes.features__right}>
                     <ProgramsItem
-                        label={'День на резорте'}
-                        subLabel={'Мы отвезем вас в один из отелей-резортов, где вы сможете насладиться ' +
-                            'беззаботным отдыхом в райском месте'}
+                        label={t(`items.${3}.label`)}
+                        subLabel={t(`items.${3}.subLabel`)}
                         imageUrl={resortDay} side={'right'}
                         openModal={openModal}
                         modalImages={[ resortDay ]}
-                        modalText={'<p>Мы сотрудничаем с несколькими отелями - резортами. Вы сможете насладиться беззаботным отдыхом, находясь целый день в райском месте, острове - отеле. Мы отвезем на своем катере вас в отель утром и заберем вечером. В стоимость входит обед с напитками во время обеда, включая алкоголь.</p><p><strong>Наши отели-партнеры:</strong></p><p>Sun Siyam Olhuveli 4*, Adaaran Club Rannalhi 4*, Fihalhohi Maldives 4*</p><p>Стоимость экскурсии "День на резорте" на человека.</p><p><span>120$</span></p><p><strong>В стоимость входит:</strong> трансфер в обе стороны, обед с напитками во время обеда. Начало в 9 утра, окончание в 17 часов вечера.</p>'}
+                        modalText={t(`items.${3}.modalText`)}
                     />
                 </div>
                 <div className={classes.features__left}>
                     <ProgramsItem
-                        label={'Романтический ужин'}
-                        subLabel={'Мы поможем красиво отметить любое ваше событие'}
+                        label={t(`items.${4}.label`)}
+                        subLabel={t(`items.${4}.subLabel`)}
                         imageUrl={romantic} side={'left'}
                         openModal={openModal}
                         modalImages={[ romantic ]}
-                        modalText={'<p>Мы поможем красиво отметить любое ваше событие - предложение руки и сердца, юбилей, день рождения или другой праздник. Организуем красивый романтический ужин на пляже на закате.</p>'}
+                        modalText={t(`items.${4}.modalText`)}
                     />
                     <ProgramsItem
-                        label={'Культура Мальдив'}
-                        subLabel={'Насладитесь национальными танцами, песнями, и мастер-классами на нашем острове'}
+                        label={t(`items.${5}.label`)}
+                        subLabel={t(`items.${5}.subLabel`)}
                         imageUrl={culture} side={'left'}
                         openModal={openModal}
                         modalImages={[ culture, culture1 ]}
-                        modalText={'<p>Мы сможем скрасить ваше пребывание на нашем острове национальными танцами, песнями, организовать мастер-классы по изготовлению мальдивского сувенира, а так же фотосессию в национальных костюмах.</p><p>Еще мы можем организовать караоке на крыше нашего отеля, вечеринку, ужин для команды, настольные игры, уроки рисования, водные виды развлечений и спорта, дайвинг, массажи, лечебные процедуры, поездку на соседние локальные острова, экскурсию в столицу нашей страны Мале.</p><p>Стоимость рассчитывается индивидуально. Спрашивайте расчет у наших менеджеров по указанным на сайте телефонам.</p>'}
+                        modalText={t(`items.${5}.modalText`)}
                     />
                 </div>
             </div>
             <div className={classes.features__description}>
                 <Description>
-                    В каждой нашей программе включено проживание в отеле.
+                    {t('description')}
                 </Description>
             </div>
             <ModalPanel/>
