0a53bed3b3d5d8dbe53a69e9e140dfcc04074e27
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-24 11:02:52 +0300

    Инициализация languagedetector

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,16 +2,20 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import global_ru from '~/translations/ru/global.json';
 import global_en from '~/translations/en/global.json';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n.use(initReactI18next).init({
-    interpolation: { escapeValue: false },
-    lng: 'ru',
-    resources: {
-        en: {
-            global: global_en,
+i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+        fallbackLng: 'en',
+        interpolation: { escapeValue: false },
+        resources: {
+            en: {
+                global: global_en,
+            },
+            ru: {
+                global: global_ru,
+            },
         },
-        ru: {
-            global: global_ru,
-        },
-    },
-});
+    });
